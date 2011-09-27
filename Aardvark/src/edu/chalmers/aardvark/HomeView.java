4c7614a3f6d37be9cdeb0c6e08c05b16a1a51b01
Magnus <waveisgay@gmail.com>
2011-09-27 19:52:20 +0200

    home added.

---
+++ Aardvark/src/edu/chalmers/aardvark/HomeView.java
@@ -0,0 +1,15 @@
+package edu.chalmers.aardvark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HomeView extends Activity {
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
