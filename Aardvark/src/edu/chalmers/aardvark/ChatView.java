dd8bfc3707526a6e93abeeea2dcee612c3079376
Magnus <waveisgay@gmail.com>
2011-09-27 17:19:53 +0200

    Added some classes.

--- Aardvark/src/edu/chalmers/aardvark/ChatView.java
+++ Aardvark/src/edu/chalmers/aardvark/ChatView.java
@@ -1,6 +1,14 @@
 package edu.chalmers.aardvark;
 
-public class ChatView {
-	//test
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatView extends Activity{
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 
 }
