45ef8952695ef1258d372581eb976c0bab55049c
Johannes Keinestam <johannes.keinestam@gmail.com>
2011-09-30 19:33:27 +0200

    User/contact related classes in model done.

--- Aardvark/src/edu/chalmers/aardvark/model/ContactsContainer.java
+++ Aardvark/src/edu/chalmers/aardvark/model/ContactsContainer.java
@@ -1,5 +1,39 @@
 package edu.chalmers.aardvark.model;
 
-public class ContactsContainer {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ContactsContainer {
+	private List<Contact> contactList;
+	
+	public ContactsContainer() {
+		contactList = new ArrayList<Contact>();
+	}
+	
+	public void addContact(Contact contact) {
+		contactList.add(contact);
+	}
+	
+	public void removeContact(Contact contact) {
+		contactList.remove(contact);
+	}
+	
+	public Contact findContact(String nickname) {
+		for (Contact c : contactList) {
+			if (c.getNickname().equals(nickname)) {
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	public Contact findContactByID(String aardvarkID) {
+		for (Contact c : contactList) {
+			if (c.getAardvarkID().equals(aardvarkID)) {
+				return c;
+			}
+		}
+		return null;
+	}
+	
 }
--- Aardvark/src/edu/chalmers/aardvark/model/User.java
+++ Aardvark/src/edu/chalmers/aardvark/model/User.java
@@ -1,5 +1,19 @@
 package edu.chalmers.aardvark.model;
 
 public class User {
-
+	private String alias;
+	private String aardvarkID;
+	
+	public User(String alias, String aardvarkID) {
+		this.alias = alias;
+		this.aardvarkID = aardvarkID;
+	}
+	
+	public String getAardvarkID() {
+		return aardvarkID;
+	}
+	
+	public String getAlias() {
+		return alias;
+	}
 }
