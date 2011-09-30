83bb67cf701b5a52ebaa757c01f27bafe19e7b92
Johannes Keinestam <johannes.keinestam@gmail.com>
2011-09-30 19:37:27 +0200

    ChatMessage class created.

--- Aardvark/src/edu/chalmers/aardvark/model/ChatMessage.java
+++ Aardvark/src/edu/chalmers/aardvark/model/ChatMessage.java
@@ -1,5 +1,33 @@
 package edu.chalmers.aardvark.model;
 
-public class ChatMessage {
+import android.text.format.Time;
 
+public class ChatMessage {
+	private String message;
+	private User user;
+	private boolean isRemote;
+	private Time timeStamp;
+	
+	public ChatMessage(String msg, User user, boolean remote, Time time) {
+		this.message = msg;
+		this.user = user;
+		this.isRemote = remote;
+		this.timeStamp = time;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public User getUser() {
+		return user;
+	}
+	
+	public boolean isRemote() {
+		return isRemote;
+	}
+	
+	public Time getTimeStamp() {
+		return timeStamp;
+	}
 }
