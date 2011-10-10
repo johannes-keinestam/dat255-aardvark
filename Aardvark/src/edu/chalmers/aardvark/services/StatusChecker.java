1cba38dd59a386db1b4a3aa8d3f86925071df77c
Johannes Keinestam <johannes.keinestam@gmail.com>
2011-10-10 19:51:37 +0200

    StatusChecker should now work, reporting logins and logouts.

--- Aardvark/src/edu/chalmers/aardvark/services/StatusChecker.java
+++ Aardvark/src/edu/chalmers/aardvark/services/StatusChecker.java
@@ -5,19 +5,15 @@ import java.util.Collection;
 import org.jivesoftware.smack.Roster;
 import org.jivesoftware.smack.RosterListener;
 import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.filter.PacketFilter;
-import org.jivesoftware.smack.filter.ToContainsFilter;
 import org.jivesoftware.smack.packet.Presence;
-
-import edu.chalmers.aardvark.model.LocalUser;
-import edu.chalmers.aardvark.util.ComBus;
-import edu.chalmers.aardvark.util.ServerConnection;
-import edu.chalmers.aardvark.util.StateChanges;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import edu.chalmers.aardvark.util.ComBus;
+import edu.chalmers.aardvark.util.ServerConnection;
+import edu.chalmers.aardvark.util.StateChanges;
 
 public class StatusChecker extends Service implements RosterListener {
 
@@ -26,38 +22,52 @@ public class StatusChecker extends Service implements RosterListener {
 	XMPPConnection connection = ServerConnection.getConnection();
 	
 	Roster roster = connection.getRoster();
+	
 	roster.setSubscriptionMode(Roster.SubscriptionMode.accept_all);
 	roster.addRosterListener(this);
 	
-	Log.i("INFO", this.toString() + " STARTED");
+	Log.i("CLASS", this.toString() + " STARTED");
     }
 
     @Override
     public void presenceChanged(Presence presence) {
 	StateChanges statusChange;
+	Log.i("STATUS", "PRESENCE CHANGED FOR USER: "+presence.getFrom());
 	if (presence.isAvailable()) {
 	    statusChange = StateChanges.USER_ONLINE;
+	    Log.i("STATUS", "LOGGED IN: "+presence.getFrom());
 	} else {
 	    statusChange = StateChanges.USER_OFFLINE;
+	    Log.i("STATUS", "LOGGED OUT: "+presence.getFrom());
 	}
 	ComBus.notifyListeners(statusChange.toString(), presence.getFrom());
     }
 
     @Override
+    public void entriesAdded(Collection<String> addresses) {
+	for (String address : addresses) {
+	    Log.i("STATUS", "USER ADDED, SETTING ONLINE: "+address);
+	    String aardvarkID = address.substring(0, address.lastIndexOf("@"));
+	    ComBus.notifyListeners(StateChanges.LOGGED_IN.toString(), aardvarkID);
+	}
+    }
+
+    @Override
+    public void entriesDeleted(Collection<String> addresses) {
+	for (String address : addresses) {
+	    Log.i("STATUS", "USER DELETED, SETTING OFFLINE: "+address);
+	    String aardvarkID = address.substring(0, address.lastIndexOf("@"));
+	    ComBus.notifyListeners(StateChanges.LOGGED_IN.toString(), aardvarkID);
+	}
+    }
+
+    @Override
+    public void entriesUpdated(Collection<String> addresses) {
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
 	return null;
-    }
-
-    @Override
-    public void entriesAdded(Collection<String> arg0) {
-    }
-
-    @Override
-    public void entriesDeleted(Collection<String> arg0) {
-    }
-
-    @Override
-    public void entriesUpdated(Collection<String> arg0) {
     }
 
 }
